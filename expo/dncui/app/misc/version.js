4f73c45d8ee75c3785ec769ccdce273d5583ce2a
shashidharanA <shashidharan736@gmail.com>
2023-01-09 12:25:01 +0530

    file removed

--- expo/dncui/app/misc/version.js
+++
@@ -1,7 +0,0 @@
-const appconst = require('./constants.js');
-
-module.exports = function (app) {
-    app.get('/version', function(req, res) {
-        res.status(200).json(""+appconst.APP_NAME+" v"+appconst.APP_VERSION);
-    });
-}
